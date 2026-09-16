b0728e054cbcf9e5acd26b3dc4d213d9f30f0db7
Ahmad zuainat <Ahmadzuainat@gmail.com>
2026-09-16 20:03:21 +0300

    First Homework

--- dart/first_homework.dart
+++ dart/first_homework.dart
@@ -1,2 +1,38 @@
-//  if else else_if 
-// loop: for, while, do_while 
+void main() {
+  double mark = 20;
+
+  switch (mark) {
+    case > 80:
+      print("A");
+      break;
+
+    case > 70:
+      print("B");
+      break;
+
+    case > 60:
+      print("C");
+      break;
+
+    case > 50:
+      print("Pass");
+      break;
+
+    default:
+      print("Failed");
+  }
+
+  // do while
+
+  int i = 0;
+  int sum = 0;
+
+  do {
+    sum += i;
+
+    print('sum = ${sum - i} + $i = $sum');
+
+    i++;
+  } while (i < 5);
+}
+
